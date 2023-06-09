77d2cfad508cd63fc66dbbb52021d7e79da794d9
Lucia Darneris <luciadarneris@gmail.com>
2023-06-09 13:02:30 -0300

    Declaración test verificar clase Producto

---
+++ src/VerificarClaseProducto.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VerificarClaseProducto {
+
+	@Test
+	void test() {
+		Producto producto = new Producto();
+		
+		assertNotNull(producto);
+	}
+
+}
