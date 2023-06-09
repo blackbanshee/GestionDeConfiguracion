7ef460c7ac689fc036f97d68b471a87223c6074c
Lucia Darneris <luciadarneris@gmail.com>
2023-06-09 15:47:17 -0300

    prueba 55544

--- src/Producto.java
+++ src/Producto.java
@@ -1,18 +1,12 @@
 
 public class Producto {
-	private String nombre;
 	private double precio;
+
 	public double getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
 }
